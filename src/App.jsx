78c75cf53bd127db03c8e1d5eaeb758a789f904c
Mahliyo <gasandigital13@gmail.com>
2026-06-20 17:36:09 +0500

    Sign up star

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,15 @@ import React from 'react'
 function App() {
   return (
     <div className="cards">
-      <h1 > </h1>
+      <h1 >Sign Up </h1>
      <form>
         <div className="input-group">
             <label htmlFor="name">Username</label>
-            <input type="text"required placeholder="Username" />
+            <input id="name" type="text"required placeholder="Username" />
       </div>
       <div className="input-group">
-            <label>Email</label>
-            <input type="email" required placeholder="you@example.com" />
+            <label htmlFor="email">Email</label>
+            <input id="email" type="email" required placeholder="you@example.com" />
           </div>
       <div className="input-group">
             <label>Password</label>
