57cdbf391811c390f1f6109b55e759656d6a9339
Mahliyo <gasandigital13@gmail.com>
2026-06-20 17:34:47 +0500

    Sign up star

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,18 @@ function App() {
       <h1 > </h1>
      <form>
         <div className="input-group">
-      <label htmlFor="name">Username</label>
-      <input type="text"required placeholder="Username" />
+            <label htmlFor="name">Username</label>
+            <input type="text"required placeholder="Username" />
       </div>
       <div className="input-group">
             <label>Email</label>
-            <input type="email" placeholder="you@example.com" />
+            <input type="email" required placeholder="you@example.com" />
           </div>
+      <div className="input-group">
+            <label>Password</label>
+            <input type="password"required placeholder="********" />
+          </div>
+          <button type="submit" className="submit-btn">Sign Up</button>
      </form>
     
     </div>
