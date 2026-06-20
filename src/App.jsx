a4470ff7fd71095ac69b0011a646d15f12ab6b1b
Mahliyo <gasandigital13@gmail.com>
2026-06-20 17:31:19 +0500

    Sign up star

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,20 @@ import React from 'react'
 
 function App() {
   return (
-    <>
+    <div className="cards">
+      <h1 > </h1>
      <form>
-      <label htmlFor="name">Ismingisni kt</label>
+        <div className="input-group">
+      <label htmlFor="name">Username</label>
+      <input type="text"required placeholder="Username" />
+      </div>
+      <div className="input-group">
+            <label>Email</label>
+            <input type="email" placeholder="you@example.com" />
+          </div>
      </form>
     
-    </>
+    </div>
   )
 }
 
