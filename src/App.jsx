8858a9e805339b2b0ccace35e1898e48f4dc2636
Mahliyo <gasandigital13@gmail.com>
2026-06-20 17:47:55 +0500

    Sign up star

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,30 @@
 import "./ App.css";
-import React from 'react'
+import React, { useState } from 'react'
 
 function App() {
+  const {name, setName} = useState("");
+  const {email, setEmail} = useState("");
+  const {password, setPassword} = useState("");
+  const bosish = (event)=>{
+    event.preventDefualt();
+  }
+
+  }
   return (
     <div className="cards">
       <h1 className="title">Sign Up </h1>
      <form>
         <div className="input-group">
             <label htmlFor="name">Username</label>
-            <input id="name" type="text"required placeholder="Username" />
+            <input value={name} onChange={(e)=>setName(e.target.value)} id="name" type="text"required placeholder="Username" />
       </div>
       <div className="input-group">
             <label htmlFor="email">Email</label>
-            <input id="email" type="email" required placeholder="you@example.com" />
+            <input value={name} onChange={(e) =>setEmail(e.target.value)} id="email" type="email" required placeholder="you@example.com" />
           </div>
       <div className="input-group">
             <label htmlFor="password">Password</label>
-            <input id="password" type="password"required placeholder="********" />
+            <input value={password} onChange={(e)=>setPassword(e.target.value)} id="password" type="password"required placeholder="********" />
           </div>
           <button type="submit" className="submit-btn">Sign Up</button>
      </form>
