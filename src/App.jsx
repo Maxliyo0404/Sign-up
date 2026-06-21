a96517d7649c2fb76ac7874cef68d0a195fe24c5
Mahliyo <gasandigital13@gmail.com>
2026-06-21 21:17:40 +0500

    apConent css

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,18 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import "./Sign.css";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AppContent from './AppContent/AppContent';
+import Sign from './Sign/Sign';
 
-function Sign() {
+function App() {
   return (
-    <div className="sign">
-        {/* Rasmga mos asosiy xabar */}
-        <h1>Xush kelibsiz!</h1>
-        
-        {/* Qo'shimcha matn (masalan, foydalanuvchini yo'naltirish uchun) */}
-        <p className="subtitle">Sizning ro'yxatdan o'tishingiz muvaffaqiyatli yakunlandi.</p>
-        
-        {/* Asosiy tugma */}
-        <div className="actions">
-            <Link to="/" className="back-link">ORTGA QAYTISH</Link>
-            
-            {/* Qo'shimcha tugma (masalan, profilga o'tish yoki dashboard) */}
-            <Link to="/profile" className="profile-link">Profilni ko'rish</Link>
-        </div>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<AppContent />} />
+        <Route path="/sign" element={<Sign />} />
+      </Routes>
+    </Router>
   );
 }
 
-export default Sign;
+export default App;
--- src/Sign/Sign.jsx
+++ src/Sign/Sign.jsx
@@ -1,13 +1,23 @@
+import React from 'react';
 import { Link } from 'react-router-dom';
 import "./Sign.css";
 
 function Sign() {
   return (
     <div className="sign">
+        {/* Rasmga mos asosiy xabar */}
         <h1>Xush kelibsiz!</h1>
-
-        <Link to="/">ORTGA QAYTISH</Link>
+        
+       
+        <p className="subtitle">Sizning ro'yxatdan o'tishingiz muvaffaqiyatli yakunlandi.</p>
+        
+        <div className="actions">
+            <Link to="/" className="back-link">ORTGA QAYTISH</Link>
+        
+            <Link to="/profile" className="profile-link">Profilni ko'rish</Link>
+        </div>
     </div>
-  )
+  );
 }
+
 export default Sign;
